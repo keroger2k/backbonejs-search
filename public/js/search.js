7abdec17d61fe4a02c77f7e4167fc7667739be9f
Kyle Rogers <kyle.rogers@gmail.com>
2011-09-05 08:53:25 -0500

    I'm much happier with this solution.  The only think I really don't like is the app.navigate()

--- public/js/search.js
+++ public/js/search.js
@@ -1,126 +1,196 @@
-(function($) {
+/// <reference path="jquery-1.6.2-vsdoc.js" />
+jQuery.download = function(url, data, method){
+	//url and data options required
+	if( url && data ){ 
+		//data can be string of parameters or array/object
+		data = typeof data == 'string' ? data : jQuery.param(data);
+		//split params into form inputs
+		var inputs = '';
+		jQuery.each(data.split('&'), function(){ 
+			var pair = this.split('=');
+			inputs+='<input type="hidden" name="'+ pair[0] +'" value="'+ pair[1] +'" />'; 
+		});
+		//send request
+		jQuery('<form action="'+ url +'" method="'+ (method||'post') +'">'+inputs+'</form>')
+		.appendTo('body').submit().remove();
+	};
+};
 
-  window.File = Backbone.Model.extend({
 
+(function ($) {
+  
+  File = Backbone.Model.extend({ 
+  
   });
 
-  window.Files = Backbone.Collection.extend({
-    model: File,
-  });
-
-  window.FilesView = Backbone.View.extend({
-    tagName: 'ol',
-    className: 'search-results',
-
-    events: {
-      "change input[type=checkbox]": "checkChange"
-    },
-    
-    initialize: function() {
-      _.bindAll(this, 'render');
-      this.template = $('#files-template');
-    },
-
-    render: function() {
-      var html = this.template.tmpl(this.collection.toJSON());
-      $(this.el).html(html);
-      return this;
-    },
-
-    checkChange: function(evt) {
-      console.log(this.model);
-    }
-
-  });
-
-  window.Query = Backbone.Model.extend({
-    
+  Query = Backbone.Model.extend({
+    url: "/search",
     defaults: {
       text: '',
       count: 0,
       time: '00:00'
     },
 
-    url: function() {
-      return 'search?q=' + this.get('text'); 
+    initialize: function() {
+      this.bind('change:text', this.render);
     },
 
     parse: function(response) {
-      this.set({
-        count: response.Count,
-        time: response.Time,
-      });
-      this.files.reset(response.Files);
-    },
-
-    initialize: function() {
-      this.bind('change:text', this.changedText);
-      this.files = new Files();
-    },
-
-    changedText: function() {
-      this.fetch();
-    }
-
-  });
-
-  window.QueryView = Backbone.View.extend({
-
-    events: {
-      'submit' : 'search'
-    },
-
-    initialize: function() {
-      _.bindAll(this, 'render', 'updateQueryResults');
-      this.template = $('#queryview-template');
-      this.filesView = new FilesView({
-        collection: this.model.files,
-        model: new Query()
-      });
+      files.reset(response.Files);
+      query.set({ count: response.Count, time: response.Time });
     },
 
     render: function() {
-      var html = this.template.tmpl(this.model.toJSON());
-      $(this.el).html(html);
-      return this;
-    },
-
-    search: function(e) {
-      e.preventDefault();
-      this.model.set({ 
-        text: $('#search').val() 
-      });
-      window.App.navigate('search/' + this.model.get('text'), true);
+      var text = this.get('text');
+      this.fetch({ data: { q: text } });
     }
 
   });
 
-  window.Google= Backbone.Router.extend({
+  Files = Backbone.Collection.extend({
+    model: File,
+  });
+  
+  window.query = new Query();
+  window.files = new Files();
 
-    routes: {
-      '': 'home',
-      'search/:q': 'search'
+  HelpView = Backbone.View.extend({
+    
+    events: {
+      "keydown" : "show",
+      "click #help-view span.wi" : "close",
+      "click" : "nofocus"
     },
 
     initialize: function() {
-      this.queryView = new QueryView();
+      this.menu = $('#help-view');
     },
 
-    home: function() {
-      var $container = $('#container');
-      $container.append(this.queryView.render().el);
+    nofocus: function(e) {
+      if(this.menu.is(':visible') && $(e.target).parents('#' + this.menu.attr('id')).length != 1) {
+        this.menu.toggle();
+      }
     },
 
-    search: function(q) {
-      var $container = $('container');
-      $container.append(this.queryView.filesView.render().el);
+    close: function() {
+      this.menu.toggle();
+    },
+
+    show: function(e) {
+      if(e.shiftKey && e.keyCode === 191 && e.target.type != "text") {
+        this.menu.toggle();
+      }
+    }
+
+  });
+  
+  ResultsView = Backbone.View.extend({
+    
+    initialize: function() {
+      this.template = $('#file-template');
+      this.collection.bind('reset', this.render, this);
+    },
+
+    render: function() {
+      var content = this.template.tmpl(this.collection.toJSON());
+      $(this.el).html(content);
+      return this;
     }
 
   });
 
-  $(function() {
-    window.App = new Google();
+  StatsView = Backbone.View.extend({
+    
+    initialize: function() {
+      this.template = $('#stats-template');
+      this.model.bind('change:time', this.render, this);
+    },
+
+    render: function() {
+      var content = this.template.tmpl(this.model.toJSON());
+      $(this.el).html(content);
+      return this;
+    }
+
+  });
+
+  MenuView = Backbone.View.extend({
+    
+    initialize: function() {
+      this.template = $('#menu-template');
+      this.model.bind('change:count', this.render, this);
+    },
+
+    render: function() {
+      $(this.el).empty();
+      if(this.model.get('count') > 0 ) {
+        var content = this.template.tmpl();
+        $(this.el).html(content);
+      }
+      return this;
+    }
+
+  });
+
+  SearchFormView = Backbone.View.extend({
+    
+    initialize: function() {
+      this.template = $('#form-template');
+    },
+
+    events: {
+      'submit #search-form' : 'query'
+    },
+
+    render: function() {
+      var content = this.template.tmpl(this.model.toJSON());
+      $(this.el).html(content);
+      return this;
+    },
+
+    query: function(e) {
+      e.preventDefault();
+      app.navigate("search/" + $('#search').val(), true);
+    }
+
+  });
+
+  Google = Backbone.Router.extend({
+    
+    initialize: function() {
+      var query = new Query();
+      this.formView = new SearchFormView({ model: window.query, el: $("#search-form") });
+      this.resultsView = new ResultsView({ model: window.query, collection: window.files, el: $("#search-results") });
+      this.helpView = new HelpView({ el: $("body") });
+      this.statsView = new StatsView({ model: window.query, el: $('#result-stats') });
+      this.menuView = new MenuView({ model: window.query, el: $('#leftnav') });
+    },
+
+    routes: {
+      '' : 'index',
+      'search/:q' : 'search'
+    },
+
+    index: function() {
+      this.formView.render();
+    },
+
+    search: function(q) {
+      window.query.set({ text: q });
+      this.formView.render();
+    }
+
+  });
+
+  $(function () {
+    app = new Google();
     Backbone.history.start();
+
+    $('body').ajaxError(function(e) {
+      console.log(e);
+    });
+
   });
 
 })(jQuery);
+
